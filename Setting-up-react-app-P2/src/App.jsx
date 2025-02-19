d8a9f148507b3962bcc06cffac5a4aedac3f045b
Abishekdevil <abishekcholan07@gmail.com>
2025-02-19 10:42:50 +0530

    updated

---
+++ Setting-up-react-app-P2/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import  './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='title'>Home Page</h1>
+      <hr></hr>
+      <div className='home'>
+        <h4>Name      : Abishek</h4>
+        <h4>Mobile No : 7598687272</h4>
+        <h4>Email     : abish12@gmail.com</h4> 
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
